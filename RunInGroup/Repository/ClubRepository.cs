4d37eb8f2abb8d400718e88a588f440bce09a7fc
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-08-31 06:58:13 +0300

    Create The Repository that Implements the Interface.

---
+++ RunInGroup/Repository/ClubRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using RunInGroup.Data;
+using RunInGroup.Data.Interface;
+using RunInGroup.Models;
+
+namespace RunInGroup.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly RunDbContext _context;
+
+        public ClubRepository(RunDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Club club)
+        {
+            _context.Add(club);
+            return save();
+        }
+
+        public bool Delete(Club club)
+        {
+            _context.Remove(club);
+            return save();
+        }
+
+        public async Task<IEnumerable<Club>> GetAllClubsAsync()
+        {
+            return await _context.Clubs.Include(c => c.Address).ToListAsync();
+        }
+
+
+        public async Task<IEnumerable<Club>> GetClubByCityAsync(string city)
+        {
+            return await _context.Clubs.Include(c=>c.Address).Where(c=>c.Address.City == city).ToListAsync();
+        }
+
+
+
+        public async Task<Club?> GetClubByIdAsync(int id)
+        {
+            if (id == 0) // I have leant this Tip to avoid operator overloading so i'm using "Is" not "=="
+            {
+                return null; 
+            }
+
+            var club = await _context.Clubs.Include(c => c.Address).FirstOrDefaultAsync(c => c.Id == id);
+            return club;
+        }
+
+
+        public bool save()
+        {
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool Update(Club club)
+        {
+            _context.Update(club);
+            return save();
+
+        }
+    }
+}
---
+++ RunInGroup/Repository/RaceRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunInGroup.Data;
+using RunInGroup.Data.Interface;
+using RunInGroup.Models;
+
+namespace RunInGroup.Repository
+{
+    public class RaceRepository :IRaceRepository
+    {
+        private readonly RunDbContext _context;
+
+        public RaceRepository(RunDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Race race)
+        {
+            _context.Add(race);
+            return SaveChanges();
+        }
+
+        public bool Delete(Race race)
+        {
+            _context.Remove(race);
+            return SaveChanges();
+        }
+
+        public async Task<IEnumerable<Race>> GetAllRacesAsync()
+        {
+            return await _context.Races.Include(c=>c.Address).ToListAsync();  
+        }
+
+        public async Task<Race> GetRaceByIdAsync(int id)
+        {
+            if (id <= 0)
+            {
+                return null; // Or you can throw an exception here.
+            }
+
+            var race = await _context.Races.Include(r => r.Address).FirstOrDefaultAsync(r => r.Id == id);
+            return race;
+        }
+
+
+        public async Task<IEnumerable<Race>> GetRacesByCityAsync(string city)
+        {
+            return await _context.Races.Include(c=>c.Address).Where(c=>c.Address.City == city).ToListAsync();
+        }
+
+        public bool SaveChanges()
+        {
+            _context.SaveChanges();
+            return true;
+
+        }
+
+        public bool Update(Race race)
+        {
+            _context.Update(race);
+            return SaveChanges();
+        }
+    }
+}
