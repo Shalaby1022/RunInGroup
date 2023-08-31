cc84f15be675460444a75a28efc5578f0af0fc01
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-08-31 06:42:37 +0300

    Refacrtor Controllers After Creating Rpositiories and Interfaces & using Async

---
+++ RunInGroup/Controllers/ClubController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunInGroup.Data;
+using RunInGroup.Data.Interface;
+
+namespace RunInGroup.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly IClubRepository _clubRepository;
+        private readonly RunDbContext _context;
+
+        public ClubController(RunDbContext context, IClubRepository clubRepository)
+        {
+            _context = context;
+            _clubRepository = clubRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var clubs = await _clubRepository.GetAllClubsAsync();
+            return View(clubs);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id) 
+        {
+            var clubs = await _clubRepository.GetClubByIdAsync(id);
+            return View(clubs);
+        }
+    }
+}
---
+++ RunInGroup/Controllers/RaceController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunInGroup.Data;
+using RunInGroup.Data.Interface;
+
+namespace RunInGroup.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly RunDbContext _context;
+        private readonly IRaceRepository _raceRepository;
+        public RaceController(IRaceRepository raceRepository, RunDbContext context)
+        {
+            _raceRepository = raceRepository;
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var races = _raceRepository.GetAllRacesAsync();
+            return View(races);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var races = await _raceRepository.GetRaceByIdAsync(id);
+            return View(races);
+        }
+
+    }
+}
