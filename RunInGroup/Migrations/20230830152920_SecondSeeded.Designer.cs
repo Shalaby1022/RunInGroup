4d88b6bbb2991dd0b71cbbcdf95f9ff05f1ad0a7
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-08-31 06:57:24 +0300

    Adding Migration and Update The DataBase

--- RunInGroup/Migrations/20230830152920_SecondSeeded.Designer.cs
+++ RunInGroup/Migrations/20230830152920_SecondSeeded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RunInGroup.Data;
 
@@ -11,9 +12,11 @@ using RunInGroup.Data;
 namespace RunInGroup.Migrations
 {
     [DbContext(typeof(RunDbContext))]
-    partial class RunDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230830152920_SecondSeeded")]
+    partial class SecondSeeded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RunInGroup/Migrations/20230830152920_SecondSeeded.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RunInGroup.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondSeeded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_clubs_Addresss_AddressId",
+                table: "clubs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_clubs_AppUser_AppUserId",
+                table: "clubs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_clubs",
+                table: "clubs");
+
+            migrationBuilder.RenameTable(
+                name: "clubs",
+                newName: "Clubs");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_clubs_AppUserId",
+                table: "Clubs",
+                newName: "IX_Clubs_AppUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_clubs_AddressId",
+                table: "Clubs",
+                newName: "IX_Clubs_AddressId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Clubs",
+                table: "Clubs",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clubs_Addresss_AddressId",
+                table: "Clubs",
+                column: "AddressId",
+                principalTable: "Addresss",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clubs_AppUser_AppUserId",
+                table: "Clubs",
+                column: "AppUserId",
+                principalTable: "AppUser",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clubs_Addresss_AddressId",
+                table: "Clubs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clubs_AppUser_AppUserId",
+                table: "Clubs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Clubs",
+                table: "Clubs");
+
+            migrationBuilder.RenameTable(
+                name: "Clubs",
+                newName: "clubs");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Clubs_AppUserId",
+                table: "clubs",
+                newName: "IX_clubs_AppUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Clubs_AddressId",
+                table: "clubs",
+                newName: "IX_clubs_AddressId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_clubs",
+                table: "clubs",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_clubs_Addresss_AddressId",
+                table: "clubs",
+                column: "AddressId",
+                principalTable: "Addresss",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_clubs_AppUser_AppUserId",
+                table: "clubs",
+                column: "AppUserId",
+                principalTable: "AppUser",
+                principalColumn: "Id");
+        }
+    }
+}
