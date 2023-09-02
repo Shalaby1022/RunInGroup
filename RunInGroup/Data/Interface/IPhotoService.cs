8ecb18faac0aaea8e10efee4057da1a11f891b15
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-09-02 17:52:53 +0300

    Creating Service to implement the refrence Methods

---
+++ RunInGroup/Data/Interface/IPhotoService.cs
@@ -0,0 +1,17 @@
+﻿using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+using Microsoft.Extensions.Options;
+
+
+
+
+namespace RunInGroup.Data.Interface
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhototAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(String publicId);
+
+
+    }
+}
