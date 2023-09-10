d7d10446c492713f516563d6ca96d94d97af9d26
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-09-10 16:34:12 +0300

    Adding Identity Framwework!

--- RunInGroup/Data/RunDbContext.cs
+++ RunInGroup/Data/RunDbContext.cs
@@ -1,17 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
-using RunInGroup.Data.Enums;
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using RunInGroup.Models;
 
 namespace RunInGroup.Data
 {
-    public class RunDbContext : DbContext
+
+    public class RunDbContext : IdentityDbContext<AppUser>
     {
+
 
        public DbSet<Race> Races { get; set; } 
         public DbSet<Club> Clubs { get; set; }
-        public DbSet<Address> Addresss { get; set; }   
-        
-   
+        public DbSet<Address> Addresss { get; set; }
+
+
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -22,9 +26,16 @@ namespace RunInGroup.Data
         {
         }
 
+      
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            base.OnModelCreating(modelBuilder);
+
+            // Configure the primary key for IdentityUserLogin
+            modelBuilder.Entity<IdentityUserLogin<string>>()
+                .HasKey(l => new { l.LoginProvider, l.ProviderKey });
+
         }
     }
 }
