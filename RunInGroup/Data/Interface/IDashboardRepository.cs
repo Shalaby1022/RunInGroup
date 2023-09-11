becf221c8f0eb37348cd35bd06d1a32a1a41673a
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-09-11 17:51:03 +0300

    creating an interface for dashboard

---
+++ RunInGroup/Data/Interface/IDashboardRepository.cs
@@ -0,0 +1,11 @@
+﻿using RunInGroup.Models;
+
+namespace RunInGroup.Data.Interface
+{
+    public interface IDashboardRepository
+    {
+        public Task<List<Club>> GetAllClubsAsync();
+        public Task<List<Race>> GetAllRacesAsync();  
+
+    }
+}
