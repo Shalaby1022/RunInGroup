216d8bfc0b0ed9e48c3cdb4cf9e652395c596dbd
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-09-12 16:46:51 +0300

    Adding ViewModels for Controller user for both index and details actions

---
+++ RunInGroup/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunInGroup.Data;
+using RunInGroup.Data.Interface;
+using RunInGroup.ViewModel;
+using System.Reflection.Metadata.Ecma335;
+
+namespace RunInGroup.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly RunDbContext _context;
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(RunDbContext context , IUserRepository userRepository)
+        {
+            _context = context;
+            _userRepository = userRepository;
+        }
+
+
+        
+        public async Task<IActionResult> Index()
+        {
+            var usres = await _userRepository.GetAllAppUser();
+            List<UsersViewModel> users = new List<UsersViewModel>();
+
+            foreach (var item in usres)
+            {
+                var userviewmodel = new UsersViewModel
+                {
+                    Id = item.Id,
+                    UserName = item.UserName,
+                    Pace = item.Pace,
+                    Mileage = item.Mileage
+                };
+                users.Add(userviewmodel);
+            }
+            return View(users);
+
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            // Retrieve the user based on the provided id
+            var user = await _userRepository.GetUserId(id);
+
+            if (user != null)
+            {
+                // Create a single user details view model
+                var userdetailsviewmodel = new UsersDetailsViewModel
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Pace = user.Pace,
+                    Mileage = user.Mileage
+                };
+
+                // Add the user details view model to the list
+                var usersDetailsViewModels = new List<UsersDetailsViewModel> { userdetailsviewmodel };
+
+                // Return the view with the populated user details view model list
+                return View(usersDetailsViewModels);
+            }
+
+            // Handle the case where the user with the provided id was not found
+            return NotFound(); // You can return a 404 page or handle it as needed.
+        }
+
+
+    }
+}
---
+++ RunInGroup/ViewModel/UsersDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RunInGroup.ViewModel
+{
+    public class UsersDetailsViewModel
+    {
+        public string Id { get; set; }
+        public string? UserName { get; set; }
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+    }
+}
